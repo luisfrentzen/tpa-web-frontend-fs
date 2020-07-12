10cb6337a3d4a20253b70b8e492ac292d2e8b4a8
Luis <luisfrentzen@gmail.com>
2020-07-12 23:50:31 +0700

    Third Commit

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -29,23 +29,57 @@ export class NavbarComponent implements OnInit {
 
   currentPage = 'home';
 
+  users = []
+
   signOut() {
+    // this.authService.signOut(true);
+    // sessionStorage.clear();
+    // // gapi.auth2.getAuthInstance().disconnect()
+
+    this.removeUser();
     this.authService.signOut();
-    sessionStorage.clear();
-    gapi.auth2.getAuthInstance().disconnect()
-    this.token = null;
     window.location.reload();
   }
 
   signInWithGoogle(){
     this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
-  }
 
-  ngOnInit(){
     this.authService.authState.subscribe(user => {
       this.user = user;
       this.loggedIn = (user != null);
+
+      this.addToLocalStorage(user);
     });
+
+  }
+
+  addToLocalStorage(user){
+    this.users.push(user);
+    localStorage.setItem('users', JSON.stringify(this.users));
+  }
+
+  getUserFromStorage(){
+    this.users = JSON.parse(localStorage.getItem('users'));
+    this.user = this.users[0];
+    this.loggedIn = true;
+  }
+
+  removeUser(){
+    window.localStorage.clear();
+    this.loggedIn = false;
+  }
+
+  ngOnInit(){
+    // this.authService.renewTokens();
+    // this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+
+    if(localStorage.getItem('users') == null){
+      this.users = [];
+    }
+    else{
+      this.getUserFromStorage();
+    }
+
 
     this.settingsVisible = false;
     this.menuVisible = false;
@@ -56,9 +90,11 @@ export class NavbarComponent implements OnInit {
     this.data.currentRestriction.subscribe(onRestriction => this.onRestriction = onRestriction)
     this.data.currentKeyboardModal.subscribe(showKeyboard => this.showKeyboard = showKeyboard)
     this.data.currentPage.subscribe(currentPage => this.currentPage = currentPage)
+    // console.log(gapi);
   }
 
   showSettings() {
+    console.log(this.authService.authState);
     this.settingsVisible = !this.settingsVisible
   }
 
